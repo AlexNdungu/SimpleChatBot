0e2b86e014d127079ecd8b88b72d7ffe5c60ce71
AlexNdungu <alexmeta517@gmail.com>
2023-03-28 20:24:40 +0300

    Test The bot

--- upgradBot.py
+++ upgradBot.py
@@ -1,6 +1,6 @@
 #Import the needed classes
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
 
 # Create and train the chatbot
 my_bot = ChatBot(name='PyBot', read_only = True, logic_adapters = ['chatterbot.logic.MathematicalEvaluation','chatterbot.logic.BestMatch'])
@@ -31,7 +31,16 @@ math_talk_1 = ['pythagorean theorem','a squared plus b squared equals c squared.
 math_talk_2 = ['law of cosine', 'c**c = a**2 + b**2 - 2 * a * b * cos(gamma)']
 
 #Train the bot
+
 list_trainer = ListTrainer(my_bot)
 
-for item in (small_talk, math_talk_1, math_talk_2):
-    list_trainer.train(item)
+#for item in (small_talk, math_talk_1, math_talk_2):
+    #list_trainer.train(item)
+
+
+#Use corpus data
+corpus_trainer = ChatterBotCorpusTrainer(my_bot)
+corpus_trainer.train('chatterbot.corpus.english')
+
+#Test the bot using get_response
+print(my_bot.get_response("hi"))
